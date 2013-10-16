bf710ddd366bc0ec74d7df5b7687e7b511717ec3
Lars Aksel <larsat@stud.hist.no>
2013-10-16 12:18:24 +0200

    Endret GUI-logikk

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -7,6 +7,7 @@ package gui;
 import java.io.File;
 import javax.swing.JOptionPane;
 import org.lwjgl.*;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.openal.AL;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -139,4 +140,89 @@ public class Client implements Runnable {
         }
         k.runGame();
     }
+    
+    /*
+     * Returns width of window...
+     */
+    public int getWindowWidth() {
+        return this.widthWindow;
+    }
+
+    /*
+     * Returns height of window...
+     */
+    public int getWindowHeight() {
+        return this.heightWindow;
+    }
+    
+    /*
+     * Is used to check for global input, this may be moved...
+     */    
+    protected void checkGlobalInput() {
+        if (Keyboard.isKeyDown(Keyboard.KEY_F11)) {
+            this.fullscreen = !this.fullscreen;
+            this.setDisplayMode(this.heightWindow, this.widthWindow, this.fullscreen);
+        }
+    }
+        
+    /**
+    * Set the display mode to be used 
+    * 
+    * @param width The width of the display required
+    * @param height The height of the display required
+    * @param fullscreen True if we want fullscreen mode
+    */
+   public void setDisplayMode(int width, int height, boolean fullscreen) {
+
+       // return if requested DisplayMode is already set
+       if ((Display.getDisplayMode().getWidth() == width) && 
+           (Display.getDisplayMode().getHeight() == height) && 
+           (Display.isFullscreen() == fullscreen)) {
+               return;
+       }
+
+       try {
+           DisplayMode targetDisplayMode = null;
+
+           if (fullscreen) {
+               DisplayMode[] modes = Display.getAvailableDisplayModes();
+               int freq = 0;
+
+               for (int i=0;i<modes.length;i++) {
+                   DisplayMode current = modes[i];
+
+                   if ((current.getWidth() == width) && (current.getHeight() == height)) {
+                       if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+                           if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+                               targetDisplayMode = current;
+                               freq = targetDisplayMode.getFrequency();
+                           }
+                       }
+
+                       // if we've found a match for bpp and frequence against the 
+                       // original display mode then it's probably best to go for this one
+                       // since it's most likely compatible with the monitor
+                       if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+                           (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+                               targetDisplayMode = current;
+                               break;
+                       }
+                   }
+               }
+           } else {
+               targetDisplayMode = new DisplayMode(width,height);
+           }
+
+           if (targetDisplayMode == null) {
+               System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+               return;
+           }
+
+           Display.setDisplayMode(targetDisplayMode);
+           Display.setFullscreen(fullscreen);
+
+       } catch (LWJGLException e) {
+           System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+       }
+   }
 }
